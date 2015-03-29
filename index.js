8e1cb217b2b8e1ab65b98e69f101ce95cd8d9a92
Erick Merchant <erickmerchant@gmail.com>
2015-03-28 20:55:55 -0700

    Drop the second signature

--- index.js
+++ index.js
@@ -2,38 +2,32 @@ var engine = require('static-engine');
 
 module.exports = function (name, plugins) {
 
-    if(!Array.isArray(plugins) || arguments.length > 2) {
-
-        plugins = [].slice.call(arguments, 1);
-    }
-
     return function (pages) {
 
-        return Promise.all(
+        pages = pages.map(function (page) {
 
-            pages.map(function (page) {
+            return new Promise(function(resolve, reject){
 
-                return new Promise(function(resolve, reject){
+                var pluginsCopy = plugins.slice(0);
 
-                    var pluginsPlus = plugins.slice(0);
+                if(page[name] && Array.isArray(page[name])) {
 
-                    if(page[name] && Array.isArray(page[name])) {
+                    pluginsCopy.unshift(function(pages, done) {
 
-                        pluginsPlus.unshift(function(pages, done) {
+                        done(null, page[name]);
+                    });
+                }
 
-                            done(null, page[name]);
-                        });
-                    }
+                engine(pluginsCopy).then(function (pages) {
 
-                    engine(pluginsPlus).then(function (pages) {
+                    page[name] = pages[0];
 
-                        page[name] = pages[0];
+                    resolve(page);
+                })
+                .catch(reject);
+            });
+        });
 
-                        resolve(page);
-                    })
-                    .catch(reject);
-                });
-            })
-        );
+        return Promise.all(pages);
     };
 };
