32ad638118bb23902fc607f94045d1e2deb1ee48
Erick Merchant <erickmerchant@gmail.com>
2015-04-10 22:31:12 -0700

    Adopt JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-var engine = require('static-engine');
+var engine = require('static-engine')
 
 module.exports = function (name, plugins) {
+  return function (pages) {
+    pages = pages.map(function (page) {
+      return new Promise(function (resolve, reject) {
+        var pluginsCopy = plugins.slice(0)
 
-    return function (pages) {
+        if (page[name] && Array.isArray(page[name])) {
+          pluginsCopy.unshift(function (pages, done) {
+            done(null, page[name])
+          })
+        }
 
-        pages = pages.map(function (page) {
+        engine(pluginsCopy).then(function (pages) {
+          page[name] = pages[0]
 
-            return new Promise(function(resolve, reject){
+          resolve(page)
+        })
+        .catch(reject)
+      })
+    })
 
-                var pluginsCopy = plugins.slice(0);
-
-                if(page[name] && Array.isArray(page[name])) {
-
-                    pluginsCopy.unshift(function(pages, done) {
-
-                        done(null, page[name]);
-                    });
-                }
-
-                engine(pluginsCopy).then(function (pages) {
-
-                    page[name] = pages[0];
-
-                    resolve(page);
-                })
-                .catch(reject);
-            });
-        });
-
-        return Promise.all(pages);
-    };
-};
+    return Promise.all(pages)
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,57 +1,51 @@
-var assert = require('assert');
-var collection = require('../index.js');
+var assert = require('assert')
+var collection = require('../index.js')
+var describe = require('mocha').describe
+var it = require('mocha').it
 
-describe('plugin', function(){
+describe('plugin', function () {
+  var plugin = collection('test', [function (pages) {
+    pages.push('a')
 
-    var plugin = collection('test', [function(pages){
+    return Promise.resolve(pages)
+  }])
 
-        pages.push('a');
+  it('should act on all desired properties as if they are pages', function (done) {
+    var promise = plugin([{}, {}, {}])
 
-        return Promise.resolve(pages);
-    }]);
+    assert.ok(promise instanceof Promise)
 
-    it('should act on all desired properties as if they are pages', function(done){
+    promise.then(function (pages) {
+      assert.deepEqual(pages, [ { test: ['a'] }, { test: ['a'] }, { test: ['a'] } ])
 
-        var promise = plugin([{}, {}, {}]);
+      done()
+    })
+      .catch(done)
+  })
 
-        assert.ok(promise instanceof Promise);
+  it('should append to existing property that is an array', function (done) {
+    var promise = plugin([{test: ['b']}])
 
-        promise.then(function(pages){
+    assert.ok(promise instanceof Promise)
 
-            assert.deepEqual(pages, [ { test: ['a'] }, { test: ['a'] }, { test: ['a'] } ]);
+    promise.then(function (pages) {
+      assert.deepEqual(pages, [ { test: ['b', 'a'] } ])
 
-            done();
-        })
-        .catch(done);
-    });
+      done()
+    })
+      .catch(done)
+  })
 
-    it('should append to existing property that is an array', function(done){
+  it('should replace existing property that is not an array', function (done) {
+    var promise = plugin([{test: 'a'}])
 
-        var promise = plugin([{test: ['b']}]);
+    assert.ok(promise instanceof Promise)
 
-        assert.ok(promise instanceof Promise);
+    promise.then(function (pages) {
+      assert.deepEqual(pages, [ { test: ['a'] } ])
 
-        promise.then(function(pages){
-
-            assert.deepEqual(pages, [ { test: ['b', 'a'] } ]);
-
-            done();
-        })
-        .catch(done);
-    });
-
-    it('should replace existing property that is not an array', function(done){
-
-        var promise = plugin([{test: 'a'}]);
-
-        assert.ok(promise instanceof Promise);
-
-        promise.then(function(pages){
-
-            assert.deepEqual(pages, [ { test: ['a'] } ]);
-
-            done();
-        })
-        .catch(done);
-    });
-});
+      done()
+    })
+      .catch(done)
+  })
+})
