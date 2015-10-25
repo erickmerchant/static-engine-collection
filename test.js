ac799d58250c7d2e2558d9e757b100fcd11270c6
Erick Merchant <erickmerchant@gmail.com>
2015-10-25 02:27:10 -0700

    Switch tests to tap

--- test.js
+++ test.js
@@ -1,51 +1,47 @@
-var assert = require('assert')
 var plugin = require('./index.js')
-var describe = require('mocha').describe
-var it = require('mocha').it
+var tap = require('tap')
 
-describe('plugin', function () {
-  var collection = plugin('test', [ function (pages) {
-    pages.push('a')
+var collection = plugin('test', [ function (pages) {
+  pages.push('a')
 
-    return Promise.resolve(pages)
-  } ])
+  return Promise.resolve(pages)
+} ])
 
-  it('should act on all desired properties as if they are pages', function (done) {
-    var promise = collection([{}, {}, {}])
+tap.test('should act on all desired properties as if they are pages', function (t) {
+  var promise = collection([{}, {}, {}])
 
-    assert.ok(promise instanceof Promise)
+  t.ok(promise instanceof Promise)
 
-    promise.then(function (pages) {
-      assert.deepEqual(pages, [ { test: ['a'] }, { test: ['a'] }, { test: ['a'] } ])
+  promise.then(function (pages) {
+    t.deepEqual(pages, [ { test: ['a'] }, { test: ['a'] }, { test: ['a'] } ])
 
-      done()
-    })
-      .catch(done)
+    t.end()
   })
+  .catch(t.end)
+})
 
-  it('should append to existing property that is an array', function (done) {
-    var promise = collection([{test: ['b']}])
+tap.test('should append to existing property that is an array', function (t) {
+  var promise = collection([{test: ['b']}])
 
-    assert.ok(promise instanceof Promise)
+  t.ok(promise instanceof Promise)
 
-    promise.then(function (pages) {
-      assert.deepEqual(pages, [ { test: ['b', 'a'] } ])
+  promise.then(function (pages) {
+    t.deepEqual(pages, [ { test: ['b', 'a'] } ])
 
-      done()
-    })
-      .catch(done)
+    t.end()
   })
+  .catch(t.end)
+})
 
-  it('should replace existing property that is not an array', function (done) {
-    var promise = collection([{test: 'a'}])
+tap.test('should replace existing property that is not an array', function (t) {
+  var promise = collection([{test: 'a'}])
 
-    assert.ok(promise instanceof Promise)
+  t.ok(promise instanceof Promise)
 
-    promise.then(function (pages) {
-      assert.deepEqual(pages, [ { test: ['a'] } ])
+  promise.then(function (pages) {
+    t.deepEqual(pages, [ { test: ['a'] } ])
 
-      done()
-    })
-      .catch(done)
+    t.end()
   })
+  .catch(t.end)
 })
